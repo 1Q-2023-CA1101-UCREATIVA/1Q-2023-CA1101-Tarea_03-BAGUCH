416edbeb7631be886772fbab448f50212c2a91c0
BAGUCH <124837173+BAGUCH@users.noreply.github.com>
2023-02-22 23:15:19 +0000

    tarea numero #03

---
+++ numeros_pares.py
@@ -0,0 +1,57 @@
+
+
+
+#for i in rango (1,20+1):
+  #if i %2 == 0:
+        #print(i)
+
+
+
+
+
+menu={
+"hamburgesa":{
+    ingredientes= ["pan", "tomate", "lechuga", "mantequilla", "salsa"]
+
+ for ingrediente in ingredientes:
+     print(ingrediente)
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+
+
+  
+
+
+
+
+
+
+        
+
